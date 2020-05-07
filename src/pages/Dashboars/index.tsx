b2ea56a94455dead1cee5dfae6d46b0e1364d202
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-05-07 17:51:39 -0300

    Input and button style

--- src/pages/Dashboars/index.tsx
+++ src/pages/Dashboars/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Title } from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title> Navegue pelo Github </Title>;
+  return (
+    <>
+      <img src={logoImage} alt="logo" />
+      <Title> Explore repositórios pelo Github </Title>
+
+      <Form>
+        <input placeholder="informe o repositório" />
+        <button type="submit">Procurar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;

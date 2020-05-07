a9b7e66065fd807610fcdc7ec08ef18cc571b31e
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-05-07 19:57:05 -0300

    Dashboard style

--- src/pages/Dashboars/index.tsx
+++ src/pages/Dashboars/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 import logoImage from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -10,9 +11,60 @@ const Dashboard: React.FC = () => {
       <Title> Explore repositórios pelo Github </Title>
 
       <Form>
-        <input placeholder="informe o repositório" />
+        <input placeholder="Informe o repositório" />
         <button type="submit">Procurar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4"
+            alt="Nilton Pedro"
+          />
+          <div>
+            <strong>niltonpedro/devops</strong>
+            <p>teste de fazer essa teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4"
+            alt="Nilton Pedro"
+          />
+          <div>
+            <strong>niltonpedro/devops</strong>
+            <p>teste de fazer essa teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4"
+            alt="Nilton Pedro"
+          />
+          <div>
+            <strong>niltonpedro/devops</strong>
+            <p>teste de fazer essa teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4"
+            alt="Nilton Pedro"
+          />
+          <div>
+            <strong>niltonpedro/devops</strong>
+            <p>teste de fazer essa teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };

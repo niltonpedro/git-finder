4dc005ed44d59a42f5cc8317dcafc69e6881bef7
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-05-09 12:23:34 -0300

    styles on details page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { Header, RepositoryInfo } from './styles'
+import { Header, RepositoryInfo, Issues } from './styles'
 import logoImage from '../../assets/logo.svg';
-import { FiChevronLeft } from 'react-icons/fi'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 
 interface RepositoryParams {
@@ -24,16 +24,59 @@ const Repository: React.FC = () => {
       </Header>
 
       <RepositoryInfo>
-        <img src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4" alt=""/>
-        <div>
-          <strong>
-            Niltonpedr/repo
-          </strong>
-          <p>
-            Sampaio
-          </p>
-        </div>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/38008073?s=460&u=be068b6a0fc43a8a498dea4f52c4d6df2aad0896&v=4" alt=""/>
+          <div>
+            <strong> Niltonpedr/repo </strong>
+            <p> Sampaio </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong> 1080 </strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong> 48 </strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong> 67 </strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
       </RepositoryInfo>
+
+      <Issues>
+        <a href="/testes">
+          <div>
+            <strong>gostack-desafio-conceitos-react-native</strong>
+            <p>Nilton Pedro</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/testes">
+          <div>
+            <strong>gostack-desafio-conceitos-react-native</strong>
+            <p>Nilton Pedro</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/testes">
+          <div>
+            <strong>gostack-desafio-conceitos-react-native</strong>
+            <p>Nilton Pedro</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/testes">
+          <div>
+            <strong>gostack-desafio-conceitos-react-native</strong>
+            <p>Nilton Pedro</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Issues>
     </>
   );
 };
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -24,29 +24,109 @@ export const Header = styled.header`
   }
 `;
 
-export const RepositoryInfo = styled.div`
+export const RepositoryInfo = styled.section`
   display: flex;
-  align-items: center;
+  margin-top: 80px;
+  flex-direction: column;
+
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+    margin-left: 24px;
+
+    strong {
+      font-size: 36px;
+      color: #3d3d4d;
+    }
+    p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+    }
+  }
+
+  }
+
+  ul {
+      display: flex;
+      list-style: none;
+      margin-top: 40px;
+
+      li {
+
+        & + li {
+          margin-left: 80px;
+        }
+
+        strong {
+          display: block;
+          font-size: 36px;
+          color: #3d3d4d;
+        }
+
+        span {
+          display: block;
+          font-size: 14px;
+          margin-top: 4px;
+          color: #6c6c80;
+        }
+
+      }
+    }
+
+`;
+
+export const Issues = styled.div`
   margin-top: 80px;
 
 
-  img {
-    width: 64px;
-    height: 64px;
-    border-radius: 50%;
-  }
+  a {
+    display: flex;
+    flex-direction: row;
+    text-decoration: none;
+    align-items: center;
 
-  div {
-    margin-left: 20px;
-    strong {
-      font-size: 20px;
-      color: #3d3d4d;
+    background: #fff;
+    width: 100%;
+    padding: 24px;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
     }
 
-    p {
-        font-size: 14px;
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 18px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 12px;
         color: #a8a8b3;
         margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;

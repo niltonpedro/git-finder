6a7860371b6e2278994c5855bb58393ba7f05693
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-05-08 21:16:15 -0300

    Routes configuration

--- src/pages/Dashboars/index.tsx
+++ src/pages/Dashboars/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logoImage from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -73,7 +74,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -84,7 +85,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Dashboard} />
-    <Route path="/repositories" component={Repository} />
+    <Route path="/repository/:id+" component={Repository} />
   </Switch>
 );
 
